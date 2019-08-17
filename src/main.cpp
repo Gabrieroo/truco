7e5100d480acf9496e61b6ec2ba666d14091b758
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2019-08-17 15:48:01 -0300

    :bento: adds cards and background

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 
-#include "./core/sdl/sdl.h"
-#include "./core/operators/pipe.h"
+#include "./core/sdl/SDL.h"
+#include "./core/operators/Pipe.h"
 
 auto print = [](auto &&... args) -> void { (std::cout << ... << args) << '\n'; };
 
 auto main() -> int
 {
-  sdl::createWindow(800, 600);
+  sdl::createWindow(1280, 720);
 
   auto texture = sdl::loadTexture("assets/background.jpg");
+
+  auto card = sdl::loadTexture("assets/sword-1.jpg");
 
   while (true)
   {
     sdl::startFrame();
 
-    sdl::paintScreen(0, 0, 0);
-
     sdl::events();
 
     sdl::texture(texture);
+    sdl::texture(card);
 
     sdl::endFrame();
   }
